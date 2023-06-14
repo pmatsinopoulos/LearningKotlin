728782cbb214a4c02ad1ab07257a9dfad8d78dac
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-14 12:00:16 +0300

    Use the Elvis operator or null coalescing operator with let

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -3,11 +3,9 @@ import java.util.*
 fun main() {
     var beverage = readLine()
     beverage = null
-    if (beverage != null) {
-        beverage = beverage.capitalize()
-    } else {
-        println("I can't do that without crashing - beverage was null!")
-    }
+    beverage?.let {
+        it.capitalize()
+    } ?: println("I can't do that without crashing - beverage was null")
     var beverageServed: String = beverage ?: "Buttered Ale"
     println(beverageServed)
 }
