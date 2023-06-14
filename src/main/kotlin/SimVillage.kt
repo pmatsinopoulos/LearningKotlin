bfc38ab15534d0b3002edd4a01c4740698e9acc3
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-14 10:35:34 +0300

    Use a variable of function type with parameter

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,6 +1,7 @@
 fun main() {
-    println({
+    val greetingFunction: (String) -> String = { playerName ->
         val currentYear = 2018
-        "Welcome to SimVillage, Mayor! (copyright $currentYear)"
-    }())
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+    println(greetingFunction("Guyal"))
 }
