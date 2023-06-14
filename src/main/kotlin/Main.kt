3e086617e7ded71c572f6a7b519f51ab4a3bd15a
Panos Matsinopoulos <panos@mixlr.com>
2023-06-14 07:45:10 +0300

    Use the range '..' operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,18 +14,18 @@ fun main(args: Array<String>) {
     }
     println(auraColor)
 
-    val healthStatus = if (healthPoints == 100) {
-        " is in excellent condition!"
-    } else if (healthPoints >= 90) {
-        " has a few scratches."
-    } else if (healthPoints >= 75) {
+    val healthStatus = if (healthPoints in 15..74) {
+        " looks pretty hurt."
+    } else if (healthPoints in 75..89) {
         if (isBlessed) {
             " has some minor wounds but is healing quite quickly!"
         } else {
             " has some minor wounds."
         }
-    } else if (healthPoints >= 15) {
-        " looks pretty hurt."
+    } else if (healthPoints in 90..99) {
+        " has a few scratches."
+    } else if (healthPoints == 100) {
+        " is in excellent condition!"
     } else {
         " is in awful condition!"
     }
