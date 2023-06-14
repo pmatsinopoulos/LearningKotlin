be17a813b7adde69e2db42a110eb7099c91a0bfd
Panos Matsinopoulos <panos@mixlr.com>
2023-06-14 07:54:52 +0300

    Use 'when' statement for the branch logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,20 +14,18 @@ fun main(args: Array<String>) {
     }
     println(auraColor)
 
-    val healthStatus = if (healthPoints in 15..74) {
-        " looks pretty hurt."
-    } else if (healthPoints in 75..89) {
-        if (isBlessed) {
+    val healthStatus = when (healthPoints) {
+        in 15..74 -> " looks pretty hurt"
+        in 75..89 -> if (isBlessed) {
             " has some minor wounds but is healing quite quickly!"
         } else {
             " has some minor wounds."
         }
-    } else if (healthPoints in 90..99) {
-        " has a few scratches."
-    } else if (healthPoints == 100) {
-        " is in excellent condition!"
-    } else {
-        " is in awful condition!"
+
+        in 90..99 -> " has a few scratches."
+        100 -> " is in excellent condition"
+        else -> " is in awful condition!"
     }
+
     println(name + healthStatus)
 }
