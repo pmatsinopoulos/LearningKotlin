b9ec0ed6f8f1c7e4a21f0e196b3f4a275deff33f
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-14 10:46:47 +0300

    Declare a function that takes as argument another function

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -4,5 +4,10 @@ fun main() {
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-    println(greetingFunction("Guyal", 2))
+    runSimulation(playerName = "Guyal", greetingFunction = greetingFunction)
+}
+
+private fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+    val numBuildings = (1..3).shuffled().last()
+    println(greetingFunction(playerName, numBuildings))
 }
