5b9806a4636abc571fd02ff3adce489646467b31
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-14 10:59:42 +0300

    Function references

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,12 +1,22 @@
 fun main() {
-    runSimulation(playerName = "Guyal") { playerName, numBuildings ->
+    runSimulation(playerName = "Guyal", costPrinter = ::printConstructionCost) { playerName, numBuildings ->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 }
 
-private inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+private inline fun runSimulation(
+    playerName: String,
+    costPrinter: (Int) -> Unit,
+    greetingFunction: (String, Int) -> String
+) {
     val numBuildings = (1..3).shuffled().last()
+    costPrinter(numBuildings)
     println(greetingFunction(playerName, numBuildings))
+}
+
+private fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
 }
