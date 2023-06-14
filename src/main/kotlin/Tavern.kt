80bbf3e02eb548934f8495d9ba57c81c8f8d4518
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-14 11:57:10 +0300

    Null coalescing operator

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,12 +1,13 @@
 import java.util.*
 
 fun main() {
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "Buttered Ale"
-        }
+    var beverage = readLine()
+    beverage = null
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage was null!")
     }
-    println("Beverage: $beverage")
+    var beverageServed: String = beverage ?: "Buttered Ale"
+    println(beverageServed)
 }
