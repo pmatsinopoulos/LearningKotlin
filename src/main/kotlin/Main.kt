f45f8aab6f6edf7c71b797a87631feb3e76272aa
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-14 09:04:54 +0300

    Organize the code in private functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,16 +4,35 @@ fun main(args: Array<String>) {
     val isBlessed = true
     val isImmortal = false
 
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-
     // Aura
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println(auraColor)
+    println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"}) $name$healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) {
         "GREEN"
     } else {
         "NONE"
     }
-    println(auraColor)
+    return auraColor
+}
 
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     val healthStatus = when (healthPoints) {
         in 15..74 -> " looks pretty hurt"
         in 75..89 -> if (isBlessed) {
@@ -26,6 +45,5 @@ fun main(args: Array<String>) {
         100 -> " is in excellent condition"
         else -> " is in awful condition!"
     }
-
-    println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"}) $name$healthStatus")
+    return healthStatus
 }
