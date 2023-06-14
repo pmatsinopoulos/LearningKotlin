84dd204c60e1fd7bd294f3270d1032541ac69728
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-14 13:34:40 +0300

    Use 'throw' to indicate an exceptional case

---
+++ src/main/kotlin/SwordJuggler.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var swordsJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if (isJugglingProficient) {
+        swordsJuggling = 2
+    }
+    proficiencyCheck(swordsJuggling)
+
+    swordsJuggling = swordsJuggling!!.plus(1)
+
+    println("You juggle $swordsJuggling swords!")
+}
+
+private fun proficiencyCheck(swordsJuggling: Int?) {
+    swordsJuggling ?: throw IllegalStateException("Player cannot juggle swords")
+}
