2db47a683165e22bf48a21e8a07c91767edae63e
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-14 14:57:23 +0300

    Example of replace with regular expressiona and lambda parsing matching part

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -12,4 +12,19 @@ private fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "Madrigal buys a $name ($type) for $price GOLD."
     println(message)
+
+    val phrase = "Ah, delicious $name!"
+    println(toDragonSpeak(phrase))
 }
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "3" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
