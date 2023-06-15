4c82d3488291f258d409bd173b066d89c20d91b2
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-15 07:58:11 +0300

    Create a List<String> and print its contents

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -5,12 +5,11 @@ const val TAVERN_NAME = "Taernyl's Folly"
 
 var playerGold = 10
 var playerSilver = 10
+val patronList: List<String> = listOf("Eli", "Mordoc", "Sophie")
 
 fun main() {
     placeOrder("shandy,Dragon's Breath,5.91")
-    placeOrder("shandy,Dragon's Breath,5.91")
-    placeOrder("shandy,Dragon's Breath,5.91")
-    placeOrder("shandy,Dragon's Breath,5.91")
+    println(patronList)
 }
 
 private fun performPurchase(price: Double) {
