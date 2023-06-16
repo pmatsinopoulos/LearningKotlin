9a5908eb9ba79224601e969f81234162641cafc7
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-16 17:02:27 +0300

    Add a generic method inside the generic class

--- src/main/kotlin/com/bignerdranch/nyethack/LootBox.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/LootBox.kt
@@ -1,7 +1,12 @@
 package com.bignerdranch.nyethack
 
 class LootBox<T>(item: T) {
+    var open = false
     private var loot: T = item
+
+    fun fetch(): T? {
+        return loot.takeIf { open }
+    }
 }
 
 class Fedora(val name: String, val value: Int)
@@ -11,4 +16,8 @@ class Coin(val value: Int)
 fun main() {
     val lootBoxOne = LootBox(item = Fedora(name = "a generic-looking Fedora", value = 15))
     val lootBoxTwo = LootBox(item = Coin(value = 15))
+    lootBoxOne.open = true
+    lootBoxOne.fetch()?.run {
+        println("name: $name, value: $value")
+    }
 }
