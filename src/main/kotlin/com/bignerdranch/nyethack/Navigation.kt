8876f79ee98ec8692e224277d670dc53fc645fed
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-16 15:17:00 +0300

    Operator overloading

--- src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
@@ -2,6 +2,8 @@ package com.bignerdranch.nyethack
 
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
+
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
 }
 
 enum class Direction(private val coordinate: Coordinate) {
@@ -10,8 +12,5 @@ enum class Direction(private val coordinate: Coordinate) {
     SOUTH(Coordinate(0, 1)),
     WEST(Coordinate(-1, 0));
 
-    fun updateCoordinate(playerCoordinate: Coordinate) = Coordinate(
-        playerCoordinate.x + coordinate.x,
-        playerCoordinate.y + coordinate.y
-    )
+    fun updateCoordinate(playerCoordinate: Coordinate) = coordinate + playerCoordinate
 }
