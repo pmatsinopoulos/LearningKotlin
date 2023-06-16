e482a37d760cfca32f69542acaa3771b353e944a
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-16 18:29:45 +0300

    Extensions on nullable types + infix keyword

--- src/main/kotlin/com/bignerdranch/nyethack/Extensions.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Extensions.kt
@@ -2,6 +2,8 @@ fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 val String.numOfVowels
     get() = count { "aeiouy".contains(it) }
 
+infix fun String?.printWhenNull(default: String) = println(this ?: default)
+
 fun <T> T.easyPrint(): T {
     println(this)
     return this
@@ -15,4 +17,8 @@ fun main() {
 
     foo.easyPrint().addEnthusiasm(5).easyPrint()
     println("Number of vowels: ${"foo".numOfVowels}")
+
+    val s: String? = null
+    s.printWhenNull("fooo")
+    s printWhenNull "foo"
 }
